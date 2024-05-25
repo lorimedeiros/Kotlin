c41ca8175ef2194a759e1ad93a6eb76c28c364bb
Lori Medeiros <98926823+lorimedeiros@users.noreply.github.com>
2024-05-24 21:19:07 -0300

    Add files via upload

---
+++ ConversorEuroDolarLinearLayout/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonConverter.setOnClickListener{
+            val euros = binding.editEuros.text.toString().trim()
+
+            if(!euros.isEmpty()){
+                val dolares = euros.toDouble() * 0.8
+                binding.textDolares.setText("${dolares}$")
+            }
+        }
+    }
+}

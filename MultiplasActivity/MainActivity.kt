38fb8a2fff2c74993ffa79fcfe851653c4fd4906
Lori Medeiros <98926823+lorimedeiros@users.noreply.github.com>
2024-05-26 15:01:18 -0300

    Add files via upload

---
+++ MultiplasActivity/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonIr.setOnClickListener {
+            /*
+            val i = Intent(this, ProfileActivity::class.java)
+            startActivity(i)
+            */
+
+            //simplificando:
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+    }
+}
